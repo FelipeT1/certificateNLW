ef1e4628ee5167669cb469992c8bf02070f4e813
FelipeT <felipeteixeiramello99@gmail.com>
2024-02-07 07:09:47 -0300

    Added and implemented basic database interaction

--- src/main/java/br/com/felipeteixeira/certificationNLW/modules/students/entities/StudentEntity.java
+++ src/main/java/br/com/felipeteixeira/certificationNLW/modules/students/entities/StudentEntity.java
@@ -1,6 +1,13 @@
 package br.com.felipeteixeira.certificationNLW.modules.students.entities;
 
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,11 +16,17 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
+    @Column(unique = true, nullable = false)
     private String email;
-    private List<CertificationStudentEntity> CertificationStudentEntity;
+
+    //private List<CertificationStudentEntity> CertificationStudentEntity;
 
 
 
